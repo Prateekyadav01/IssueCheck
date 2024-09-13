3d3ad59c94283cffbc23940dd8e313b5439a7cf8
PrateekYadav0001 <prateek.yadav@xindus.net>
2024-09-13 17:36:52 +0530

    done

--- backend/index.js
+++ backend/index.js
@@ -18,7 +18,7 @@ app.use(cookieParser());
 
 const allowedOrigins = [
     'http://localhost:5173',
-   'https://zs-shop.vercel.app'
+   'https://issue-check.vercel.app'
   ];
   
   // CORS options
