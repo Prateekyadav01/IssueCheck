3fd88db9a4b02494c475db25636de893f45984ea
PrateekYadav0001 <prateek.yadav@xindus.net>
2024-09-13 17:42:06 +0530

    done

--- backend/index.js
+++ backend/index.js
@@ -8,41 +8,44 @@ const userRouter = require("./rout/userRout");
 dotenv.config();
 const app = express();
 
+// Middleware
 app.use(cookieParser());
-
-// app.use(cors({
-//     origin: ['https://zs-shop.vercel.app/', 'http://localhost:5173/'], // Replace with your frontend's domain
-//     methods: ['GET', 'POST', 'PUT', 'DELETE'],
-//     credentials: true, // Allow credentials such as cookies
-// }));
+app.use(express.json());
 
 const allowedOrigins = [
-    'http://localhost:5173',
-   'https://issue-check.vercel.app'
-  ];
-  
-  // CORS options
-  const corsOptions = {
-    origin: (origin, callback) => {
-      if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
-        callback(null, true);
-      } else {
-        callback(new Error('Not allowed by CORS'));
-      }
-    },
-    credentials: true
-  };
+  'http://localhost:5173',
+  'https://issue-check.vercel.app'
+];
 
-  
-  app.use(cors(corsOptions));
+// CORS options
+const corsOptions = {
+  origin: (origin, callback) => {
+    if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  credentials: true, // Allow credentials such as cookies
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+};
+
+// CORS middleware
+app.use(cors(corsOptions));
+
+// Explicitly handle OPTIONS requests (preflight)
 app.options("*", cors(corsOptions));
-app.use(express.json());
+
+// Routers
 app.use(userRouter);
 
+// MongoDB connection
 mongoose.connect(process.env.MONGODB_URL)
-    .then(() => console.log("DB connected"))
-    .catch((err) => console.log(err));
+  .then(() => console.log("DB connected"))
+  .catch((err) => console.log(err));
 
+// Start server
 app.listen(process.env.PORT, () => {
-    console.log("App running on port", process.env.PORT);
+  console.log("App running on port", process.env.PORT);
 });
